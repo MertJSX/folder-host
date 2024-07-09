2b394ef98f55e452d542b1925c319cfdcda64922
MertJSX <mertJSX@proton.me>
2024-07-09 22:11:55 +0300

    Edited strings.js

--- strings.js
+++ strings.js
@@ -1,18 +1,26 @@
 const strings = {
 defaultConfig: `
 # Port is required. Don't delete it!
-port: 3000\n
+port: 5000
+
 # This is folder path. You can change it, but don't delete.
-folder: './test'\n
+folder: './test'
+
 # This is password for access. You can delete it if you want, but it's good to have a password.
-password: '123'\n
+password: '123'
+
 # You can manage user permissions.
 permissions:
     read_directories: true
     read_files: true
-    create: true
+    create: false
     change: true
     delete: true
+    move: true
+    download: true
+    upload: true
+    rename: true
+
 # Holds deleted files. Accidentally, you might delete files that you don't want to delete.
 recovery_bin: true
 `,
