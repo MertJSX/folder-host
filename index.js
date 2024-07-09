1857837a178aff3b8cda035b4d0d4c452d1629bf
MertJSX <mertJSX@proton.me>
2024-07-09 22:18:00 +0300

    Added a lot of content
    Contents: placeholders, better directory information and optimizations

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const colors = require("colors");
 const yaml = require("js-yaml");
 const { strings } = require("./strings");
+const { getTotalSize, getDirItems } = require("./utils");
 const cors = require("cors");
 let config;
 let abort = false;
@@ -23,6 +24,8 @@ if (fs.existsSync("./config.yml")) {
 } else {
     config = yaml.load(fs.readFileSync('config.yaml', 'utf8'));
 }
+
+// Get config data on start
 
 console.log(config);
 
@@ -46,116 +49,145 @@ app.use(express.static(config.folder))
 app.use(cors());
 app.use(express.json());
 
-config.permissions.read_directories ? 
-app.get("/read-dir", (req, res) => {
+// Get folder size on start
 
-    let path = "";
+console.log("Total size:".green);
+console.log(getTotalSize(config.folder));
 
-    if (req.query.folder) {
-        path = req.query.folder;
-    }
+config.permissions.read_directories ?
+    app.get("/read-dir", (req, res) => {
 
-    if (config.password) {
-        if (req.query.password !== config.password) {
-            res.status(400);
-            res.json({ err: "Wrong password!" })
-            return
+        let path = "";
+        let getFileSize = req.body.getFileSize !== undefined ?
+            req.body.optimized : true;
+
+        if (req.query.folder) {
+            path = req.query.folder;
         }
-    }
 
-    fs.readdir(`${config.folder}${path}`, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200);
-            res.json({
-                files
-            })
+        if (config.password) {
+            if (req.query.password !== config.password) {
+                res.status(400);
+                res.json({ err: "Wrong password!" })
+                return
+            }
         }
+
+        let data = getDirItems(`${config.folder}${path}`, getFileSize)
+        res.status(200);
+        res.json({
+            data: data,
+            res: "Successfully readed!"
+        })
     })
+    : console.log("/read-dir".yellow + " cancelled!".gray)
 
 
-})
- : console.log("/read-dir".yellow + " cancelled!".gray)
+config.permissions.read_files ?
+    app.get("/read-file", (req, res) => {
 
+        let filepath;
 
-config.permissions.read_files ? 
-app.get("/read-file", (req, res) => {
-
-    let filepath;
-
-    if (req.query.filepath) {
-        filepath = req.query.filepath;
-    } else {
-        res.status(400);
-        res.json({
-            err: "Bad request!"
-        })
-        return
-    }
-
-    if (config.password) {
-        if (req.query.password !== config.password) {
-            res.status(400);
-            res.json({ err: "Wrong password!" })
-            return
-        }
-    }
-
-    console.log(`${config.folder}${filepath}`);
-
-    fs.readFile(`${config.folder}${filepath}`, "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
+        if (req.query.filepath) {
+            filepath = req.query.filepath;
         } else {
-            res.status(200);
-            res.json({ data: data })
-        }
-    });
-}) : console.log("/read-file".yellow + " cancelled!".gray)
-
-app.post("/write-file", (req, res) => {
-
-    console.log(req.query);
-
-    let filepath;
-    let content;
-
-    if (req.query.filepath) {
-        filepath = req.query.filepath;
-    } else {
-        res.status(400);
-        res.json({
-            err: "Bad request!"
-        })
-        return
-    }
-
-    if (req.body.content) {
-        content = req.body.content;
-    } else {
-        res.status(400);
-        res.json({
-            err: "Bad request!"
-        })
-        return
-    }
-
-    if (config.password) {
-        if (req.query.password !== config.password) {
             res.status(400);
-            res.json({ err: "Wrong password!" })
+            res.json({
+                err: "Bad request!"
+            })
             return
         }
-    }
 
-    console.log(`${config.folder}${filepath}`);
+        if (config.password) {
+            if (req.query.password !== config.password) {
+                res.status(400);
+                res.json({ err: "Wrong password!" })
+                return
+            }
+        }
 
-    fs.writeFileSync(`${config.folder}${filepath}`, content);
+        console.log(`${config.folder}${filepath}`);
 
-    res.status(200);
-    res.json({ response: "Saved!" })
-})
+        fs.readFile(`${config.folder}${filepath}`, "utf8", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.status(200);
+                res.json({ data: data })
+            }
+        });
+    }) : console.log("/read-file".yellow + " cancelled!".gray)
+
+!config.permissions.change && !config.permissions.create ?
+    console.log("/write-file".yellow + " cancelled!".gray)
+    : app.post("/write-file", (req, res) => {
+
+        console.log(req.query);
+
+        let filepath;
+        let content;
+        let type; // create or change
+
+        // Check permissions
+
+        if (req.query.type === "change" && !config.permissions.change) {
+            res.status(403);
+            res.json({ err: "You don't have permission!" })
+            return
+        } else if (req.query.type === "create" && !config.permissions.create) {
+            res.status(403);
+            res.json({ err: "You don't have permission!" })
+            return
+        }
+
+        if (req.query.filepath && req.body.content && req.query.type) {
+            filepath = req.query.filepath;
+            content = req.body.content;
+
+            if (type === "create") {
+                type = "create";
+            } else if (type === "change") {
+                type = "change"
+            } else {
+                res.status(400);
+                res.json({
+                    err: "Bad request!"
+                })
+                return
+            }
+        } else {
+            res.status(400);
+            res.json({
+                err: "Bad request!"
+            })
+            return
+        }
+
+        if (config.password) {
+            if (req.query.password !== config.password) {
+                res.status(400);
+                res.json({ err: "Wrong password!" })
+                return
+            }
+        }
+
+        console.log(`${config.folder}${filepath}`);
+
+        if (fs.existsSync(`${config.folder}${filepath}`) && type === "create") {
+            res.status(200);
+            res.json({ response: "Already exist!" })
+            return;
+        } else if (!fs.existsSync(`${config.folder}${filepath}`) && type === "change") {
+            res.status(200)
+            res.json({ response: "The file doesn't exist!" })
+            return;
+        }
+
+        fs.writeFileSync(`${config.folder}${filepath}`, content);
+
+        res.status(200);
+        res.json({ response: "Saved!" })
+    })
 
 if (!abort) {
     app.listen(config.port, () => {
