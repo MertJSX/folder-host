0c32e546475d93d852bb24ae1771cbee3afa9218
MertJSX <mertJSX@proton.me>
2024-07-12 23:49:44 +0300

    Modified dir_item.js

--- dir_item.js
+++ dir_item.js
@@ -1,8 +1,11 @@
 class DirItem {
-    constructor(name, parentPath, isDirectory, size) {
+    constructor(name, parentPath, path, isDirectory, birthDate, dateModified, size) {
         this.name = name;
         this.parentPath = parentPath;
+        this.path = path;
         this.isDirectory = isDirectory;
+        this.birthDate = birthDate;
+        this.dateModified = dateModified;
         if (size) {
             this.size = size;
         }
