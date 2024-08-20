b0cd4ecc878c8756defc96a54e624a55b9948330
MertJSX <mertJSX@proton.me>
2024-08-20 23:57:43 +0300

    Modified log.js

--- log.js
+++ log.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 const path = require('path');
 
 async function logAction(username, text, description, config) {
-    if (!config.logs.create_item) {
+    if (!config.log_activities) {
         return
     }
     const date = new Date();
@@ -15,9 +15,9 @@ async function logAction(username, text, description, config) {
         await fs.promises.mkdir(dir, { recursive: true });
 
         if (description !== null) {
-            await fs.promises.appendFile(logPath, `[${moment(date).format("HH:mm:ss")}] ${username}: \n${text} \n|\t ${description}\n`, 'utf8');
+            await fs.promises.appendFile(logPath, `[${moment(date).format("HH:mm:ss")}] ${username}: \n|\t${text} \n|\t ${description}\n\n`, 'utf8');
         } else {
-            await fs.promises.appendFile(logPath, `[${moment(date).format("HH:mm:ss")}] ${username}: ${text} \n`, 'utf8');
+            await fs.promises.appendFile(logPath, `[${moment(date).format("HH:mm:ss")}] ${username}: ${text} \n\n`, 'utf8');
         }
     } catch (err) {
         console.error(err);
